ae17a9ee4f9e6a5951bdbc0973518c3ba1aee391
Kenneth Siu <kenneth.siu@softwire.com>
2021-11-12 11:47:04 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int n = 1; n <= 100; n++) {
+            if (n % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (n % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (n % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(n);
+            }
+        }
+    }
+}
